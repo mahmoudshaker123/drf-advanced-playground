125e668e021c4bdca25b1ede6edf4d6279db6b45
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-02-22 16:17:32 +0200

    using APIVIEW

--- drf/views.py
+++ drf/views.py
@@ -1,3 +1,18 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Article
 
 # Create your views here.
+
+def hello(request):
+    return JsonResponse({'message': 'Hello, world!'})
+
+
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        return Response({'articles': articles.values()})
+    
